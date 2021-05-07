ffc4375bbc5bfcd9dc2179f6d1cd4099ec71814d
Michel Olvera <michel.olvera.p@outlook.com>
2021-05-06 19:48:07 -0500

    Agregado Microservicio Spring Cloud Gateway que permite mapear las rutas del resto de microservicios en un solo puerto.

---
+++ gateway-service/src/main/java/dev/michel/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package dev.michel.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Microservicio Spring Cloud Gateway que permite mapear las rutas del resto de microservicios en un solo puerto.
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
---
+++ gateway-service/src/test/java/dev/michel/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.michel.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
